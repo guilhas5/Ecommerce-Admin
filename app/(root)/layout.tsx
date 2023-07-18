33b4e40e63e3f0697df6242b0dd9c1a8c98b7619
Guilherme Costa <costaguilherme1995@gmail.com>
2023-07-18 09:28:53 +0200

    starting to implement the logic for the routing

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,33 @@
+
+import React from 'react'
+import {auth} from "@clerk/nextjs"
+import { redirect } from 'next/navigation'
+import prismadb from '@/lib/prismadb'
+
+type Props = {
+  children: React.ReactNode
+}
+
+export default async function SetupLayout({children}: Props) {
+  const {userId} = auth()
+  if (!userId) {
+    redirect("/sign-in")
+  }
+
+  const store = await prismadb.store.findFirst({
+    where: {
+      userId: userId
+    }
+  }) 
+
+  if (store) {
+    redirect(`${store.id}`)
+  }
+
+
+return (
+  <>
+  {children}
+  </>
+)
+}
