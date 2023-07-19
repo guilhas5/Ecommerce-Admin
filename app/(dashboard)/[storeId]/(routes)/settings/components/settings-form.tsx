31142a672236358bf3673abc851849fe6888d45f
Guilherme Costa <costaguilherme1995@gmail.com>
2023-07-19 15:25:30 +0200

    Incrementing DELETE and Patch function to control de delete and add functionalities on the settings. Api connected to the settings-form

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -9,8 +9,20 @@ import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useParams, useRouter } from "next/navigation";
+import { AlertModal } from "@/components/modals/alert-modal";
+import { data } from "autoprefixer";
 
 interface SettingsFormProps {
   initialData: Store;
@@ -25,20 +37,60 @@ type SettingsFormValues = z.infer<typeof formSchema>;
 const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
   const [open, setOpen] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
-  
+  const params = useParams()
+  const router = useRouter()
+
   const form = useForm<SettingsFormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: initialData,
   });
 
   const onSubmit = async (data: SettingsFormValues) => {
-    console.log(data);
+    try {
+      setLoading(true)
+      await axios.patch(`/api/stores/${params.storeId}`, data)
+      router.refresh()
+      toast.success("Store updated")
+    } catch (error) {
+      toast.error("Something went wrong")
+    }finally {
+      setLoading(false)
+    }
   };
+
+  const onDelete = async () => {
+    try {
+      setLoading(true)
+      await axios.delete(`/api/stores/${params.storeId}`)
+      router.refresh()
+      router.push("/")
+      toast.success("Store deleted.")
+    } catch (error) {
+      toast.error("Make sure you removed all products and categories first.")
+      
+    }finally {
+      setLoading(false)
+      setOpen(false)
+    }
+  }
   return (
     <>
+    <AlertModal
+    isOpen={open}
+    onClose={()=> setOpen(false)}
+    onConfirm={onDelete}
+    loading = {loading}
+
+     />
       <div className="flex items-center justify-between ">
         <Heading title="Settings" description="Manage store preferences" />
-        <Button variant="destructive" size="icon" onClick={() => {}}>
+        <Button
+        
+          disabled={loading}
+          variant="destructive"
+          size="icon"
+          onClick={() => setOpen(true)}
+        >
           <Trash className="h-4 w-4" />
         </Button>
       </div>
@@ -49,19 +101,22 @@ const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
           className="space-y-8 w-full"
         >
           <div className="grid grid-cols-3 gap-8">
-            <FormField 
-            control={form.control}
-            name="name"
-            render={({field}) =>(
-              <FormItem>
-                <FormLabel>Name</FormLabel>
-                <FormControl>
-                  <Input disabled={loading} placeholder="Store name" {...field}/>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-
-            )}
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Store name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
           </div>
           <Button disabled={loading} className="ml-auto" type="submit">
---
+++ app/api/stores/[storeId]/route.ts
@@ -0,0 +1,70 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const { name } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthentucated", { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    const store = await prismadb.store.updateMany({
+      where: {
+        userId: userId,
+        id: params.storeId,
+      },
+      data: {
+        name,
+      },
+    });
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log("[STORE_PATCH]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthentucated", { status: 401 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    const store = await prismadb.store.deleteMany({
+      where: {
+        userId: userId,
+        id: params.storeId,
+      },
+    });
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log("[STORE_DELETE]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
